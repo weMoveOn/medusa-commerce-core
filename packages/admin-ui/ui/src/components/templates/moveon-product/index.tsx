86dc5c1437f468d7e153455e0d2a4ed4e576c857
RakibMiahMVN <rakib.miah@moveon.com.bd>
2023-09-24 18:05:32 +0600

    feat: 85zu0em48: add and functional modal when importing

--- packages/admin-ui/ui/src/components/templates/moveon-product/index.tsx
+++ packages/admin-ui/ui/src/components/templates/moveon-product/index.tsx
@@ -12,7 +12,7 @@ import { filterForTemporal } from "../../../utils/filterFixedData"
 import InventoryProductFilters from "../inventory-product-filter"
 import InventoryProductSort from "../inventory-product-sort"
 import useInventoryProductFilters from "../../../hooks/use-inventory-product-filter"
-import { useLocation } from "react-router-dom"
+import { useLocation, useNavigate } from "react-router-dom"
 import queryString from "query-string"
 import ArrowLeftIcon from "../../fundamentals/icons/arrow-left-icon"
 import LoadingContainer from "../../atoms/loading-container"
@@ -33,6 +33,7 @@ import { IPriceSettingReturnType } from "../../../types/inventory-price-setting"
 
 const MoveOnProduct = () => {
   const { resetInterval } = usePolling()
+  const navigate = useNavigate()
   const createBatchJob = useAdminCreateBatchJob()
   const notification = useNotification()
   const location = useLocation();
@@ -76,9 +77,9 @@ const MoveOnProduct = () => {
       (x) => x.value === selectedSortByShop.value
     )  
 
-    const { isLoading: isPriceSettingLoading, data: priceSettingData, refetch: refetchPriceSetting } = useQuery<
+    const { isLoading: isPriceSettingLoading, data: priceSettingData } = useQuery<
     AxiosResponse<IPriceSettingReturnType>
-    >(["single-price-setting-retrieve"], () =>
+    >(["single-price-setting-retrieve", selectedSortByShopData], () =>
     MedusaAPI.InventoryPriceSettings.list(selectedSortByShopData?.key))
   
   // if data is fetched from backend set new count and limit
@@ -94,6 +95,11 @@ const MoveOnProduct = () => {
     useEffect(()=>{
       submitFilter()
     }, [limit, offset])
+
+    useEffect(()=>{
+      setSelectedProducts([])
+      setMultipleImport(false)
+    }, [selectedSortByShop])
     
 
   useEffect(() => {
@@ -160,8 +166,8 @@ const MoveOnProduct = () => {
       (x) => x.title === value.label
     )
     if (selectedSortData) {
-      let key = "sortType"
-      let orderValue = "sortOrder"
+      const key = "sortType"
+      const orderValue = "sortOrder"
 
       if (selectedSortData?.key === "Default") {
         updateQueryParams({ [key]: undefined, [orderValue]: undefined })
@@ -182,7 +188,7 @@ const MoveOnProduct = () => {
       (x) => x.title === value.label
     )
     if (selectedSortByShopData) {
-      let key = "shop_id"
+      const key = "shop_id"
 
       if (selectedSortByShopData?.key === defaultMoveonInventoryFilter.shop_id) {
         updateQueryParams({ [key]: undefined})
@@ -194,6 +200,23 @@ const MoveOnProduct = () => {
       setIsParamsUpdated(true)
       refetch()
     }
+  }
+
+  const handleMultipleImport = async() =>{  
+    if(!priceSettingData?.data.count){
+const shouldImport = await dialog({
+heading: "Attention",
+text: "No price role found for this store, set at least 1 price rule to import product",
+confirmText: "Set Price Role", 
+buttonVariant: "primary"
+});
+if (!shouldImport) {
+  return
+} else {
+  navigate(`/a/settings/inventory-pricing`)
+}
+}
+   setMultipleImport(!multipleImport)
   }
 
   const handleNextPage = () => {
@@ -208,24 +231,26 @@ const MoveOnProduct = () => {
 
   const handleSelect = ({ vpid, link }: IInventoryProductSelectType) => {
     const productKey = `${vpid}_${link}`;
-    const updatedSet = new Set(selectedProducts.map(product => `${product.vpid}_${product.link}`));
   
-    if (updatedSet.has(productKey)) {
-      updatedSet.delete(productKey);
-    } else {
-      updatedSet.add(productKey);
-    }
+    setSelectedProducts(prevSelectedProducts => {
+      const isSelected = prevSelectedProducts.some(product =>
+        `${product.vpid}_${product.link}` === productKey
+      );
   
-    const updatedArray = Array.from(updatedSet).map(key => {
-      const [updatedVpid, updatedLink] = key.split('_');
-      return { vpid: updatedVpid, link: updatedLink };
+      if (isSelected) {
+        // Remove the item if it's already selected
+        return prevSelectedProducts.filter(product =>
+          `${product.vpid}_${product.link}` !== productKey
+        );
+      } else {
+        // Add the item if it's not selected
+        return [...prevSelectedProducts, { vpid, link }];
+      }
     });
-  
-    setSelectedProducts(updatedArray);
   };
+  
 
   const handleImport = async(product?: IInventoryProductSelectType) =>{
-    console.log(priceSettingData)
     const productsToImport = product ? [product] : selectedProducts;
     if(selectedProducts.length===0 && !product){
       notification("Error", "You must select at least one product to import", "warning")
@@ -234,18 +259,22 @@ const MoveOnProduct = () => {
       const selectedSortByShopData = filterForTemporal.shop.values.find(
         (x) => x.value === selectedSortByShop.value
       )  
-      const shouldImportText = !priceSettingData?.data.count  ? "You must add price rule to import product"
-  : "Are you sure you want to import this product(s) with the current price role?";
+      const shouldImportText = !priceSettingData?.data.count  ? "No price role found for this store, set at least 1 price rule to import product"
+  : "Are you sure you want to import this product(s) with current price role?";
 
 const shouldImport = await dialog({
-  heading: "Confirm Import",
+  heading: !priceSettingData?.data.count?"Attention":"Confirm Import",
   text: shouldImportText,
+  confirmText: !priceSettingData?.data.count? "Set Price Role":"Yes, confirm", 
+  buttonVariant: !priceSettingData?.data.count?"primary":"nuclear"
 });
   
       if (!shouldImport) {
         return
       }
-  
+      if(!priceSettingData?.data.count){
+        navigate(`/a/settings/inventory-pricing`)
+      }else {
       const reqObj = {
         dry_run: false,
         type: "moveOn-inventory-product-import",
@@ -269,7 +298,8 @@ const shouldImport = await dialog({
       })
     }
   }
-  
+  }
+
   return (
     <>
       <div className="container"> 
@@ -296,9 +326,10 @@ const shouldImport = await dialog({
           </div>
           <div className="px-3 py-3 flex gap-4 items-center">
           <Button
+          // loading={isPriceSettingLoading}
             variant={multipleImport?"primary":"secondary"}
             size="small"
-            onClick={() => setMultipleImport(!multipleImport)}
+            onClick={handleMultipleImport}
             >
              <DownloadIcon size={20} />
               Multiple Import
