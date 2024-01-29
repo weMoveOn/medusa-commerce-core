e4c2854b3c42e77963c3f7c53cced444720d2782
masud-rahman-mvn <masudur.rahman@moveon.com.bd>
2024-01-29 19:44:26 +0600

    stepper done

--- packages/admin-ui/ui/src/moveshop-ui/components/molecules/stepper/index.tsx
+++ packages/admin-ui/ui/src/moveshop-ui/components/molecules/stepper/index.tsx
@@ -117,48 +117,62 @@ export const SteppedProvider = ({ children }: any) => {
 }
 
 const StepActive = ({
-  isActive,
-  isLastIndex,
+  currentStep,
+  index,
 }: {
-  isActive?: boolean
-  isLastIndex?: boolean
+  currentStep: number
+  index: number
 }) => {
+  // currentStep >= index || currentStep === index
   return (
     <li
       className={clsx("flex w-full  items-center justify-between", {
-        "text-blue-600 dark:text-blue-500": true,
         // eslint-disable-next-line quotes
         'after:inline-block after:h-1 after:w-full after:border-4 after:border-b after:border-blue-100 after:content-[""] dark:after:border-blue-800':
-          true, // active line
-        "w-1/12": isLastIndex,
+          currentStep >= index,
+        // eslint-disable-next-line quotes
+        'after:inline-block after:h-1 after:w-full after:border-4 after:border-b after:border-gray-100 after:content-[""] dark:after:border-gray-800':
+          currentStep <= index,
       })}
     >
-      <span
-        className={clsx(
-          "flex h-10 w-10 shrink-0 items-center justify-center rounded-full bg-blue-100 dark:bg-blue-800 lg:h-12 lg:w-12",
-          {
-            "text-blue-600 dark:text-blue-300 lg:h-4 lg:w-4": true,
-          }
-        )}
-      >
-        <svg
-          className={clsx("h-3.5 w-3.5", {
-            "text-blue-600 dark:text-blue-300 lg:h-4 lg:w-4": true,
-          })}
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 16 12"
+      {currentStep >= index ? (
+        <span
+          className={clsx(
+            "flex h-10 w-10 shrink-0 items-center justify-center rounded-full bg-blue-100 dark:bg-blue-800 lg:h-12 lg:w-12",
+            {
+              "text-blue-600 dark:text-blue-300 lg:h-4 lg:w-4": true,
+              "text-gray-500 dark:text-gray-100 lg:h-5 lg:w-5": true,
+            }
+          )}
         >
-          <path
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M1 5.917 5.724 10.5 15 1.5"
-          />
-        </svg>
-      </span>
+          <svg
+            className={clsx(
+              "h-3.5 w-3.5 text-blue-600 dark:text-blue-300 lg:h-4 lg:w-4"
+            )}
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 16 12"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M1 5.917 5.724 10.5 15 1.5"
+            />
+          </svg>
+        </span>
+      ) : (
+        <span
+          className={clsx(
+            " h-10 w-10 shrink-0 rounded-full bg-gray-100 dark:bg-gray-700",
+            {
+              "text-gray-500 dark:text-gray-100 ": true,
+            }
+          )}
+        ></span>
+      )}
     </li>
   )
 }
@@ -188,36 +202,6 @@ const StepTitle = ({
         >
           {title}
         </span>
-      </li>
-    </>
-  )
-}
-
-const StepInactive = ({
-  isActive,
-  isLastIndex,
-}: {
-  isActive: boolean
-  isLastIndex: boolean
-}) => {
-  return (
-    <>
-      <li
-        className={clsx("flex items-center justify-between ", {
-          // eslint-disable-next-line quotes
-          'w-full after:inline-block after:h-1 after:w-full after:border-4 after:border-b after:border-gray-100 after:content-[""] dark:after:border-gray-700':
-            isLastIndex,
-          "w-1/12": isLastIndex,
-        })}
-      >
-        <span
-          className={clsx(
-            "flex h-10 w-10 shrink-0 items-center justify-center rounded-full bg-gray-100 dark:bg-gray-700 lg:h-12 lg:w-12",
-            {
-              "text-gray-500 dark:text-gray-100 lg:h-5 lg:w-5": true,
-            }
-          )}
-        ></span>
       </li>
     </>
   )
@@ -259,13 +243,13 @@ const StepperMVN: React.FC<SteppedProps> = ({
                     <div className="   ">
                       <div>
                         <ol className="flex w-full items-center justify-between">
-                          {steps?.map((_, i) => {
-                            if (context.currentStep >= i) {
-                              return <StepActive />
-                            }
-
-                            return <></>
-                          })}
+                          {steps?.map((_, i) => (
+                            <StepActive
+                              key={`step-${i}`}
+                              currentStep={context.currentStep}
+                              index={i}
+                            />
+                          ))}
                         </ol>
                         <br />
                         <ol className="flex w-full items-center justify-between">
