a74a5215622db849d0ecefdad4a7c26ec826152e
masud-rahman-mvn <masudur.rahman@moveon.com.bd>
2024-02-13 11:49:43 +0600

    added react-select

--- packages/admin-ui/ui/src/domain/onboarding/index.tsx
+++ packages/admin-ui/ui/src/domain/onboarding/index.tsx
@@ -1,19 +1,23 @@
 /* eslint-disable react/jsx-key */
 /* eslint-disable quotes */
 import React from "react"
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, useParams } from "react-router-dom"
 import { useTranslation } from "react-i18next"
 
 import { SteppedContext } from "../../components/molecules/modal/stepped-modal"
 import { LayeredModalContext } from "../../components/molecules/modal/layered-modal"
 import StepperMVN from "../../moveshop-ui/components/molecules/stepper"
 import { clx } from "../../utils/clx"
-import Panels from "../../moveshop-ui/steps/panels"
-import Example from "../../moveshop-ui/steps/circles"
+
 import IconCircle from "../../moveshop-ui/components/fundamentals/icon-circle"
 
 import { supportedLanguages } from "../../i18n"
-import { Select } from "@medusajs/ui"
+
+import { useAdminRegion } from "medusa-react"
+import Spinner from "../../components/atoms/spinner"
+
+import Select from "react-select"
+import { useStoreData } from "../settings/regions/components/region-form/use-store-data"
 
 type StepProps = {
   label: string
@@ -21,13 +25,6 @@ type StepProps = {
   subtitle: string
 }
 
-const Step = () => {
-  return (
-    <>
-      <div>step</div>
-    </>
-  )
-}
 interface ProgressStepProps {
   isComplete?: boolean
   label?: string
@@ -148,19 +145,13 @@ const StepCardBusiness = ({ icon, title }) => {
 const StepCardSell = ({ icon, title }) => {
   return (
     <>
-      <div
-        className={clx(
-          " items-center rounded-lg border border-black p-5",
-          {},
-          {}
-        )}
-      >
+      <div className={clx(" items-center rounded-lg border p-5", {}, {})}>
         <label
           htmlFor={`${title}`}
           className="mb-3  flex items-center  justify-between gap-3"
         >
           {icon}
-          <input type="checkbox" id={`${title}`} className={clx(" h-4 w-4")} />
+          <input type="checkbox" id={`${title}`} className={clx(" h-5 w-5")} />
         </label>
         <h3 className="text-large font-semibold">
           Products I buy or make myself
@@ -238,43 +229,191 @@ const Step2 = () => {
   )
 }
 
-const StepLanguageMenu: React.FC = () => {
-  const { i18n } = useTranslation()
+const RegionForm = () => {
+  const params = useParams()
+  const id: string | undefined = params["*"]
+  const { t } = useTranslation()
+  const { region, isLoading, isError } = useAdminRegion(id!, {
+    enabled: !!id,
+  })
 
-  const changeLanguage = (lng: string) => {
-    console.log("lng :>> ", lng)
-    // i18n.changeLanguage(lng)
+  if (isLoading) {
+    return (
+      <div className="flex h-full w-full items-center justify-center">
+        <Spinner variant="secondary" />
+      </div>
+    )
   }
 
-  return (
-    <div className="w-full ">
-      <Select value={i18n.language} onValueChange={changeLanguage}>
-        <Select.Trigger>
-          <Select.Value />
-        </Select.Trigger>
-        <Select.Content>
-          {supportedLanguages.map((lng) => (
-            <Select.Item key={lng.locale} value={lng.locale}>
-              {lng.name}
-            </Select.Item>
-          ))}
-        </Select.Content>
-      </Select>
-    </div>
-  )
+  if (isError) {
+    return (
+      <div className="bg-grey-0 rounded-rounded border-grey-20 gap-y-xsmall flex h-full w-full flex-col items-center justify-center border text-center ">
+        <h1 className="inter-large-semibold">
+          {t("edit-something-went-wrong", "Something went wrong...")}
+        </h1>
+        <p className="inter-base-regular text-grey-50">
+          {t(
+            "edit-no-region-found",
+            "We can't find a region with that ID, use the menu to the left to select a region."
+          )}
+        </p>
+      </div>
+    )
+  }
+
+  if (!region) {
+    return null
+  }
+
+  return <></>
 }
 
-const StepLanguageSettings = () => {
-  const { t } = useTranslation()
-
-  return (
-    <>
-      <StepLanguageMenu />
-    </>
-  )
+export interface ColourOption {
+  readonly value: string
+  readonly label: string
+  readonly color: string
+  readonly isFixed?: boolean
+  readonly isDisabled?: boolean
 }
 
+export const colourOptions: readonly ColourOption[] = [
+  { value: "ocean", label: "Ocean", color: "#00B8D9", isFixed: true },
+  { value: "blue", label: "Blue", color: "#0052CC", isDisabled: true },
+  { value: "purple", label: "Purple", color: "#5243AA" },
+  { value: "red", label: "Red", color: "#FF5630", isFixed: true },
+  { value: "orange", label: "Orange", color: "#FF8B00" },
+  { value: "yellow", label: "Yellow", color: "#FFC400" },
+  { value: "green", label: "Green", color: "#36B37E" },
+  { value: "forest", label: "Forest", color: "#00875A" },
+  { value: "slate", label: "Slate", color: "#253858" },
+  { value: "silver", label: "Silver", color: "#666666" },
+]
+
+export interface FlavourOption {
+  readonly value: string
+  readonly label: string
+  readonly rating: string
+}
+
+export const flavourOptions: readonly FlavourOption[] = [
+  { value: "vanilla", label: "Vanilla", rating: "safe" },
+  { value: "chocolate", label: "Chocolate", rating: "good" },
+  { value: "strawberry", label: "Strawberry", rating: "wild" },
+  { value: "salted-caramel", label: "Salted Caramel", rating: "crazy" },
+]
+
+export interface StateOption {
+  readonly value: string
+  readonly label: string
+}
+
+export const stateOptions: readonly StateOption[] = [
+  { value: "AL", label: "Alabama" },
+  { value: "AK", label: "Alaska" },
+  { value: "AS", label: "American Samoa" },
+  { value: "AZ", label: "Arizona" },
+  { value: "AR", label: "Arkansas" },
+  { value: "CA", label: "California" },
+  { value: "CO", label: "Colorado" },
+  { value: "CT", label: "Connecticut" },
+  { value: "DE", label: "Delaware" },
+  { value: "DC", label: "District Of Columbia" },
+  { value: "FM", label: "Federated States Of Micronesia" },
+  { value: "FL", label: "Florida" },
+  { value: "GA", label: "Georgia" },
+  { value: "GU", label: "Guam" },
+  { value: "HI", label: "Hawaii" },
+  { value: "ID", label: "Idaho" },
+  { value: "IL", label: "Illinois" },
+  { value: "IN", label: "Indiana" },
+  { value: "IA", label: "Iowa" },
+  { value: "KS", label: "Kansas" },
+  { value: "KY", label: "Kentucky" },
+  { value: "LA", label: "Louisiana" },
+  { value: "ME", label: "Maine" },
+  { value: "MH", label: "Marshall Islands" },
+  { value: "MD", label: "Maryland" },
+  { value: "MA", label: "Massachusetts" },
+  { value: "MI", label: "Michigan" },
+  { value: "MN", label: "Minnesota" },
+  { value: "MS", label: "Mississippi" },
+  { value: "MO", label: "Missouri" },
+  { value: "MT", label: "Montana" },
+  { value: "NE", label: "Nebraska" },
+  { value: "NV", label: "Nevada" },
+  { value: "NH", label: "New Hampshire" },
+  { value: "NJ", label: "New Jersey" },
+  { value: "NM", label: "New Mexico" },
+  { value: "NY", label: "New York" },
+  { value: "NC", label: "North Carolina" },
+  { value: "ND", label: "North Dakota" },
+  { value: "MP", label: "Northern Mariana Islands" },
+  { value: "OH", label: "Ohio" },
+  { value: "OK", label: "Oklahoma" },
+  { value: "OR", label: "Oregon" },
+  { value: "PW", label: "Palau" },
+  { value: "PA", label: "Pennsylvania" },
+  { value: "PR", label: "Puerto Rico" },
+  { value: "RI", label: "Rhode Island" },
+  { value: "SC", label: "South Carolina" },
+  { value: "SD", label: "South Dakota" },
+  { value: "TN", label: "Tennessee" },
+  { value: "TX", label: "Texas" },
+  { value: "UT", label: "Utah" },
+  { value: "VT", label: "Vermont" },
+  { value: "VI", label: "Virgin Islands" },
+  { value: "VA", label: "Virginia" },
+  { value: "WA", label: "Washington" },
+  { value: "WV", label: "West Virginia" },
+  { value: "WI", label: "Wisconsin" },
+  { value: "WY", label: "Wyoming" },
+]
+
+export const optionLength = [
+  { value: 1, label: "general" },
+  {
+    value: 2,
+    label:
+      "Evil is the moment when I lack the strength to be true to the Good that compels me.",
+  },
+  {
+    value: 3,
+    label:
+      "It is now an easy matter to spell out the ethic of a truth: 'Do all that you can to persevere in that which exceeds your perseverance. Persevere in the interruption. Seize in your being that which has seized and broken you.",
+  },
+]
+
+export const dogOptions = [
+  { id: 1, label: "Chihuahua" },
+  { id: 2, label: "Bulldog" },
+  { id: 3, label: "Dachshund" },
+  { id: 4, label: "Akita" },
+]
+
+// let bigOptions = [];
+// for (let i = 0; i < 10000; i++) {
+// 	bigOptions = bigOptions.concat(colourOptions);
+// }
+
+export interface GroupedOption {
+  readonly label: string
+  readonly options: readonly ColourOption[] | readonly FlavourOption[]
+}
+
+export const groupedOptions: readonly GroupedOption[] = [
+  {
+    label: "Colours",
+    options: colourOptions,
+  },
+  {
+    label: "Flavours",
+    options: flavourOptions,
+  },
+]
 const Step3 = () => {
+  const { currencyOptions, countryOptions } = useStoreData()
+
+  console.log("countryOptions :>> ", countryOptions)
   return (
     <>
       <div>
@@ -282,27 +421,17 @@ const Step3 = () => {
 
         <div className="mt-4">
           <p>Where is your business located?</p>
-          <div className="border-grey-20 py-xlarge border-t">
-            <StepLanguageSettings />
-          </div>
-        </div>
+          <br />
 
-        <div className="mt-4">
-          <label htmlFor="business">
-            <span>Where is your business located?</span>
-          </label>
-          <div className="mb-3 flex gap-3 rounded-lg border p-4">
-            <input type="checkbox" id="business" />
-            <span>Bangladesh</span>
-          </div>
-          <div className="flex gap-3 ">
-            <div className=" rounded-lg  border p-3">
-              Bangladeshi Taka (BDT)
-            </div>
-            <div className="  rounded-lg border p-3">
-              Bangladeshi Taka (BDT)
-            </div>
-          </div>
+          <Select
+            options={countryOptions}
+            isMulti
+            isSearchable
+            name="colors"
+            className="basic-multi-select"
+            classNamePrefix="select"
+          />
+          <div className="border-grey-20  "></div>
         </div>
       </div>
     </>
