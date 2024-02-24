93de4937d4fd58ae8156c67c86fa7b00a205d2e7
masud-rahman-mvn <masudur.rahman@moveon.com.bd>
2024-02-24 18:19:25 +0600

    phone menu design done

--- packages/admin-ui/ui/src/components/organisms/navbar/menu/phone-menu/index.tsx
+++ packages/admin-ui/ui/src/components/organisms/navbar/menu/phone-menu/index.tsx
@@ -1,44 +1,78 @@
-import React from "react"
+import React, { useCallback } from "react"
+import { NavLink } from "react-router-dom"
+import HomeIcon from "../../../../fundamentals/icons/home-icon"
+import OrdersIcon from "../../../../fundamentals/icons/orders-icon"
+import ProductsIcon from "../../../../fundamentals/icons/products-icon"
 
 const PhoneMenu: React.FC = () => {
   const styles =
-    "group py-1.5 my-0.5 rounded-rounded flex font-bold justify-between text-large text-grey-50 hover:bg-grey-10 items-center px-2"
-  const activeStyles = "bg-grey-10 is-active"
+    "group  flex font-bold justify-between text-large text-grey-50  items-center px-2"
+  const activeStyles = "is-active text-green-500"
   const classNameFn = useCallback(({ isActive }: any) => {
     const str = isActive ? `${styles} ${activeStyles}` : styles
     return str
   }, [])
   return (
     <>
-      <div>
-        <div className="inline-flex h-16 w-[360px] items-start justify-between bg-white px-4 shadow">
-          <div className="inline-flex shrink grow basis-0 flex-col items-center justify-center gap-1 self-stretch py-3">
-            <div className="inline-flex h-6 w-6 items-center justify-center p-0.5">
-              <div className="relative h-5 w-5"></div>
-            </div>
-            <div className="self-stretch text-center font-['Inter'] text-xs font-medium leading-none tracking-wide text-zinc-900">
-              <NavLink className={classNameFn} to={"a/home"}>
-                <div className="flex items-center gap-3">
-                  <span className="group-[.is-active]:text-grey-90">home</span>
+      <div className="fixed bottom-0 z-50 w-full ">
+        <div>
+          <div className="flex h-16   items-center justify-between border bg-white px-4  shadow-lg">
+            <NavLink className={classNameFn} to={"/a/home"}>
+              <div className="inline-flex shrink grow basis-0 flex-col items-center justify-center gap-1 self-stretch py-3">
+                <div className="inline-flex h-6 w-6 items-center justify-center p-0.5">
+                  <div className="relative h-5 w-5">
+                    <HomeIcon />
+                  </div>
                 </div>
-              </NavLink>
-            </div>
-          </div>
-          <div className="inline-flex shrink grow basis-0 flex-col items-center justify-center gap-1 self-stretch py-3">
-            <div className="inline-flex h-6 w-6 items-center justify-center p-[2.50px]">
-              <div className="relative h-[19px] w-[19px]"></div>
-            </div>
-            <div className="self-stretch text-center font-['Inter'] text-xs font-medium leading-none tracking-wide text-green-700">
-              Order
-            </div>
-          </div>
-          <div className="inline-flex shrink grow basis-0 flex-col items-center justify-center gap-1 self-stretch py-3">
-            <div className="inline-flex h-6 w-6 items-center justify-center p-0.5">
-              <div className="relative h-5 w-5"></div>
-            </div>
-            <div className="self-stretch text-center font-['Inter'] text-xs font-medium leading-none tracking-wide text-zinc-900">
-              Product
-            </div>
+                <div className="self-stretch text-center font-['Inter'] text-xs font-medium leading-none tracking-wide text-zinc-900">
+                  <div>
+                    <div className="flex items-center gap-3">
+                      <span className="group-[.is-active]:text-green-500">
+                        home
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </NavLink>
+
+            <NavLink className={classNameFn} to={"/a/orders"}>
+              <div className="inline-flex shrink grow basis-0 flex-col items-center justify-center gap-1 self-stretch py-3">
+                <div className="inline-flex h-6 w-6 items-center justify-center p-0.5">
+                  <div className="relative h-5 w-5">
+                    <OrdersIcon />
+                  </div>
+                </div>
+                <div className="self-stretch text-center font-['Inter'] text-xs font-medium leading-none tracking-wide text-zinc-900">
+                  <div>
+                    <div className="flex items-center gap-3">
+                      <span className="group-[.is-active]:text-green-500">
+                        Orders
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </NavLink>
+
+            <NavLink className={classNameFn} to={"/a/products"}>
+              <div className="inline-flex shrink grow basis-0 flex-col items-center justify-center gap-1 self-stretch py-3">
+                <div className="inline-flex h-6 w-6 items-center justify-center p-0.5">
+                  <div className="relative h-5 w-5">
+                    <ProductsIcon />
+                  </div>
+                </div>
+                <div className="self-stretch text-center font-['Inter'] text-xs font-medium leading-none tracking-wide text-zinc-900">
+                  <div>
+                    <div className="flex items-center gap-3">
+                      <span className="group-[.is-active]:text-green-500">
+                        Products
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </NavLink>
           </div>
         </div>
       </div>
--- packages/admin-ui/ui/src/components/templates/layout.tsx
+++ packages/admin-ui/ui/src/components/templates/layout.tsx
@@ -1,11 +1,10 @@
-import React from "react"
+import React, { useCallback } from "react"
 import { Toaster } from "react-hot-toast"
 import Sidebar from "../organisms/sidebar"
 import Navbar from "../organisms/navbar"
 
 import { useTranslation } from "react-i18next"
-import SidebarMenuItem from "../molecules/sidebar-menu-item"
-import HomeIcon from "../fundamentals/icons/home-icon"
+import PhoneMenu from "../organisms/navbar/menu/phone-menu"
 
 const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
   const { t } = useTranslation()
@@ -25,43 +24,7 @@ const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
         <div className="medium:block ">
           <Navbar />
         </div>
-        <div className="fixed bottom-0 z-50 w-full">
-          <div>
-            <div className="flex h-16   items-center justify-between border bg-white px-4  shadow-lg">
-              <div className="inline-flex shrink grow basis-0 flex-col items-center justify-center gap-1 self-stretch py-3">
-                <div className="inline-flex h-6 w-6 items-center justify-center p-0.5">
-                  <div className="relative h-5 w-5">
-                    <SidebarMenuItem
-                      pageLink={"/a/home"}
-                      icon={<HomeIcon />}
-                      triggerHandler={() => {}}
-                      text={t("home", "Home")}
-                    />
-                  </div>
-                </div>
-                <div className="self-stretch text-center font-['Inter'] text-xs font-medium leading-none tracking-wide text-zinc-900">
-                  Home
-                </div>
-              </div>
-              <div className="inline-flex shrink grow basis-0 flex-col items-center justify-center gap-1 self-stretch py-3">
-                <div className="inline-flex h-6 w-6 items-center justify-center p-[2.50px]">
-                  <div className="relative h-[19px] w-[19px]"></div>
-                </div>
-                <div className="self-stretch text-center font-['Inter'] text-xs font-medium leading-none tracking-wide text-green-700">
-                  Order
-                </div>
-              </div>
-              <div className="inline-flex shrink grow basis-0 flex-col items-center justify-center gap-1 self-stretch py-3">
-                <div className="inline-flex h-6 w-6 items-center justify-center p-0.5">
-                  <div className="relative h-5 w-5"></div>
-                </div>
-                <div className="self-stretch text-center font-['Inter'] text-xs font-medium leading-none tracking-wide text-zinc-900">
-                  Product
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+
         <div className="  flex  w-full ">
           <div className="medium:block hidden">
             <Sidebar />
@@ -75,6 +38,8 @@ const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
           </div>
         </div>
       </div>
+
+      <PhoneMenu />
     </>
   )
 }
